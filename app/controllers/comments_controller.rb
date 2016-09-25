9ed8e67b1caee87d93957b9ac594a34e879a6297
WangTzuHung <kcteamhk2001@gmail.com>
2016-09-24 23:00:04 -0700

    added backend voting function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,17 @@ class CommentsController < ApplicationController
     else
       render 'posts/show'
     end
+  end
+  def vote
+    @post = Comment.find(params[:id])
+    @vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
 
+    if @vote.valid?
+      flash[:success] = 'Your vote was counted!'
+    else
+      flash[:error] = "You can only vote for #{@post.title} once!"
+    end
+
+    redirect_to :back
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,19 @@ class Comment < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :post
+  has_many :votes, as: :voteable
 
   validates :content, presence: true, length: {minimum: 5}
+
+  def up_votes
+    votes.where(vote: true).length
+  end
+
+  def down_votes
+    votes.where(vote: false).length
+  end
+
+  def total_votes
+    up_votes - down_votes
+  end
 end
